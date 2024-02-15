5e28b5f2bbfaa005fe9fb9711cecd57fc3d38c90
Dale Black <djblack@uci.edu>
2024-02-15 07:42:50 -0800

    add comments and export

--- examples/TodoApp/src/todo.jl
+++ examples/TodoApp/src/todo.jl
@@ -3,38 +3,42 @@ using HTTP
 using HTMLStrings: divv, head, meta, title, link, script, body, nav, h1, form, input, button, a, p
 include("components/navbar.jl")
 
+tasks = Vector{String}()
+
+function render_tasks(tasks)
+    html_elements = ""
+    for (index, task) in enumerate(tasks)
+        html_elements *= divv(:class => "flex justify-between items-center pl-2",
+            divv(:class => "task-text", task),
+            button(:hx_post => "/todo/todos/$(index)/delete", :hx_target => "#task-list", :class => "btn btn-error w-1/4", "Remove")
+        )
+    end
+    return html_elements
+end
+
+# load any tasks
+@get "/todo/todos" function ()
+    return render_tasks(tasks)
+end
+
+# post new task
+@post "/todo/todos" function (req::HTTP.Request)
+    task_dict = HTTP.URIs.queryparams(String(req.body))
+    task = get(task_dict, "task", "")
+    task = HTTP.URIs.unescapeuri(task) # Decode URL-encoded characters
+    push!(tasks, task)
+    return render_tasks(tasks) # Render the updated task list
+end
+
+# delete task at {id}
+@post "/todo/todos/{id}/delete" function (req::HTTP.Request, id::String)
+    index = parse(Int, id)
+    deleteat!(tasks, index)
+    return render_tasks(tasks)
+end
+
+export todo
 function todo()
-    tasks = Vector{String}()
-
-    function render_tasks(tasks)
-        html_elements = ""
-        for (index, task) in enumerate(tasks)
-            html_elements *= divv(:class => "flex justify-between items-center pl-2",
-                divv(:class => "task-text", task),
-                button(:hx_post => "/todo/todos/$(index)/delete", :hx_target => "#task-list", :class => "btn btn-error w-1/4", "Remove")
-            )
-        end
-        return html_elements
-    end
-
-    @get "/todo/todos" function ()
-        return render_tasks(tasks)
-    end
-
-    @post "/todo/todos" function (req::HTTP.Request)
-        task_dict = HTTP.URIs.queryparams(String(req.body))
-        task = get(task_dict, "task", "")
-        task = HTTP.URIs.unescapeuri(task) # Decode URL-encoded characters
-        push!(tasks, task)
-        return render_tasks(tasks) # Render the updated task list
-    end
-
-    @post "/todo/todos/{id}/delete" function (req::HTTP.Request, id::String)
-        index = parse(Int, id)
-        deleteat!(tasks, index)
-        return render_tasks(tasks)
-    end
-
     return divv(
         head(
             meta(:charset => "UTF-8"),
