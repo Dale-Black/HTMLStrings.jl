8695b868f9668660014f217c8a6201ae39753b8a
Dale Black <djblack@uci.edu>
2024-02-16 08:06:06 -0800

    update pluto nbs

--- examples/PlutoExamples/notebook.jl
+++ examples/PlutoExamples/notebook.jl
@@ -1,21 +1,16 @@
 ### A Pluto.jl notebook ###
-# v0.19.26
+# v0.19.38
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 8eff8726-63b8-11ee-04e5-f91141c9d1b5
-# ╠═╡ show_logs = false
-begin
-	using Pkg
-	Pkg.activate(".")
-	using HTML: to_html, divv, p, span, h1, link, head, script, img
-end
+# ╔═╡ 23bd6d7c-d3d5-4027-82eb-197319735d40
+using HTMLStrings: to_html, divv, p, span, h1, link, head, script, img
 
 # ╔═╡ cacdad2b-afab-4e0b-9724-12021f9641ba
 to_html(
 	divv(
-		p("To use HTML.jl in a Pluto cell, you must wrap each div in a",
+		p("To use HTMLStrings.jl in a Pluto cell, you must wrap each div in a",
 			span("`to_html`"),
 			"cell"
 		)
@@ -43,7 +38,7 @@ to_html(
 				divv(:class => "card card-bordered border-primary text-center w-full dark:text-[#e6e6e6]",
 					divv(:class => "card-body flex flex-col justify-center items-center",
 						img(:src => "https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", :class => "h-10 w-full md:h-32 md:w-32 rounded-lg", :alt => "Glass Logo"),
-						divv(:class => "text-5xl font-bold bg-gradient-to-r from-accent to-primary inline-block text-transparent bg-clip-text py-10", "HTML.jl"),
+						divv(:class => "text-5xl font-bold bg-gradient-to-r from-accent to-primary inline-block text-transparent bg-clip-text py-10", "HTMLStrings.jl"),
 						p(:class => "card-text text-md font-serif", "HTML in pure Julia"
 						)
                     )
@@ -53,9 +48,34 @@ to_html(
     )
 )
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+HTMLStrings = "c47fe496-5789-4377-b1db-55e89f2ee0c6"
+
+[compat]
+HTMLStrings = "~0.1.1"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.1"
+manifest_format = "2.0"
+project_hash = "3e1720d821f7df4826de09a799ad334c07ce6757"
+
+[[deps.HTMLStrings]]
+git-tree-sha1 = "38563308e600ce3440d43141b817c915b5e39e38"
+uuid = "c47fe496-5789-4377-b1db-55e89f2ee0c6"
+version = "0.1.1"
+"""
+
 # ╔═╡ Cell order:
-# ╠═8eff8726-63b8-11ee-04e5-f91141c9d1b5
+# ╠═23bd6d7c-d3d5-4027-82eb-197319735d40
 # ╠═cacdad2b-afab-4e0b-9724-12021f9641ba
-# ╠═95a6cb58-e87c-44b9-86db-c13daa82e58c
-# ╠═b9645a69-b5b6-4fe9-a828-8bed7cd4a4cb
+# ╟─95a6cb58-e87c-44b9-86db-c13daa82e58c
+# ╟─b9645a69-b5b6-4fe9-a828-8bed7cd4a4cb
 # ╟─dbc4fb16-fcb6-4240-96b1-26aa4d6b95c3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
